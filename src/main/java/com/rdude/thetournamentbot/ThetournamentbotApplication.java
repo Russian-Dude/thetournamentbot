b25ed14710dae5e07ed7889a863e3703584f1291
Russian-Dude <ghritnik@gmail.com>
2022-10-08 14:30:16 +0300

    Initial commit

---
+++ src/main/java/com/rdude/thetournamentbot/ThetournamentbotApplication.java
@@ -0,0 +1,13 @@
+package com.rdude.thetournamentbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThetournamentbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThetournamentbotApplication.class, args);
+	}
+
+}
